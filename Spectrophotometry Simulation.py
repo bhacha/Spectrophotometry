740fa91dc45d8a68a48e42b9b9a0c5a1bf8a43d9
Brandon Hacha <bgh1997@gmail.com>
2020-04-19 23:34:42 -0500

    Update Spectrophotometry Simulation.py

--- Spectrophotometry Simulation.py
+++ Spectrophotometry Simulation.py
@@ -8,15 +8,25 @@ from matplotlib.figure import Figure
 from matplotlib import pyplot as plt
 import numpy as np
 
+
+
+option = tk.Tk()
+option.wm_title("Spectrophotometry Parameters")
+optionWindow = tk.ttk.Frame(option, padding="6 6 12 12")
+optionWindow.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
+optionWindow.columnconfigure(0, weight=10)
+optionWindow.rowconfigure(0, weight=1)
+
+
 # All the variable placeholders///////////////////////////////////////////////////////
 
 NumberOfPoints = int(1e5)
 spectrum = np.zeros(NumberOfPoints)
-spectralMode = 'full'
-concentration = 1 #molar
-wlMin = 400 #nm
-wlMax = 800
-#wlrange = np.linspace(1,1000,100)
+photonsabsorbed = spectrum
+spectralMode = 'full'#molar
+concentration = 0
+concentrationString = tk.StringVar()
+wlrange = np.linspace(1,1000,100)
 
 #////////////////////////////////////////////////////////////////////////////////////////
 def wlRange(*args):
@@ -35,14 +45,38 @@ def wlSingle(*args):
     except ValueError:
         pass
 
-    
+def Absorbance(wavelength,CrossSection,HWHM,conc):
+    global spectrum 
+    global photonsabsorbed
+    global wlrange
+    global phosec
+    gamma = HWHM
+    wavenumsource = 1/(wlrange/1e7)
+    omega = wavenumsource
+    Concentration = conc
+    concMolecules = Concentration*6.02e23
+    N = concMolecules/1000 #molecules per cm^3
+    sigmap = CrossSection #cm^2
+    peaknm = wavelength #nm
+    peak = peaknm*1e-9#m
+    peakwn = 1/(peaknm*1e-7)
+    omegao = peakwn
+    sigmaw = (sigmap*gamma**2)/((gamma**2)+((omegao-omega)**2))#normalized to peak area
+    intensitys = intensity*np.exp((-(sigmaw)*path*N))
+    phosecs = phosec*np.exp((-(sigmaw)*path*N)) #photons per second per cm^2 transmitted through 
+    peakfreq = c/peak
+    freqbw = peakfreq*((peak+(2e-9))-(peak-2e-9))/(peak)
+    spectrum = spectrum+np.log10(phosec/phosecs)
+    photonsabsorbed = photonsabsorbed + (phosec-phosecs)
 
 def spectrumAbsorbance(*args):
     global spectrum 
     global photonsabsorbed
-    global peakfreq
-    global freqbw
+    global phosec
+    global wlrange
+    global concentration
     try:
+        concentration = float(concentrationString.get())
         wavenumsource = 1/(wlrange/1e7)
         phosec = (wlrange*intensity)/(h*c) #photons per second per cm**2
         omega = wavenumsource
@@ -58,6 +92,10 @@ def spectrumAbsorbance(*args):
         sigmaw = (sigmap*gamma**2)/((gamma**2)+((omegao-omega)**2))#normalized to peak area
         phosecs = phosec*np.exp((-(sigmaw)*path*Nmolecule)) #photons per second per cm^2 transmitted through 
         spectrum = np.log10(phosec/phosecs)
+#This line adds interfering species/////////////////////////////////////////////////////////////////
+        #Absorbance(Peak Wavelength, Cross Section, HWHM in wavenumbers, Concentration)
+        Absorbance(250,1e-20,20000,1)
+#///////////////////////////////////////////////////////////////////////////////////////////////    
         plotSpectrum(spectrum)
     except ValueError:
         pass
@@ -68,8 +106,8 @@ def singleAbsorbance(*args):
     global photonsabsorbed
     global peakfreq
     global freqbw
+    global CrossSection
     try:
-        global value
         wavenumsource = 1/(wlrange/1e7)
         phosec = (wlrange*intensity)/(h*c) #photons per second per cm**2
         omega = wavenumsource
@@ -84,7 +122,7 @@ def singleAbsorbance(*args):
         omegao = peakwn
         sigmaw = (sigmap*gamma**2)/((gamma**2)+((omegao-omega)**2))#normalized to peak area
         intensitys = intensity*np.exp((-(sigmaw)*path*Nmolecule))
-        phosecs = phosec*np.exp((-(sigmaw)*path*N)) #photons per second per cm^2 transmitted through 
+        phosecs = phosec*np.exp((-(sigmaw)*path*Nmolecule)) #photons per second per cm^2 transmitted through 
         value = np.log10(phosec/phosecs)
         
     except ValueError:
@@ -102,12 +140,6 @@ def giveResults(*args):
         pass
     
 
-option = tk.Tk()
-option.wm_title("Spectrophotometry Parameters")
-optionWindow = tk.ttk.Frame(option, padding="6 6 12 12")
-optionWindow.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-optionWindow.columnconfigure(0, weight=10)
-optionWindow.rowconfigure(0, weight=1)
 
 #wavelength setting options//////////////////////////////////////////////////
 wlMin = tk.StringVar()
@@ -133,7 +165,7 @@ ttk.Radiobutton(optionWindow, text="Full Spectrum", variable = spectralMode, val
 #///////////////////////////////////////////////////////////////////////
 
 #User Parameter Input////////////////////////////////////////////////////////////
-concentration_entry = tk.ttk.Entry(optionWindow, width=7, textvariable=concentration)
+concentration_entry = tk.ttk.Entry(optionWindow, width=7, textvariable=concentrationString)
 concentration_entry.grid(column=2, row=3, sticky=(tk.W, tk.E))
 
 ttk.Label(optionWindow, text="concentration (M)").grid(column=1, row=3, sticky='w')
@@ -146,7 +178,7 @@ ttk.Label(optionWindow, text="concentration (M)").grid(column=1, row=3, sticky='
 
 #Hidden Parameters///////////////////////////////////////////////////
 
-PathLength = .1 #cm
+PathLength = 1 #cm
 LaserPower = 10e-3 #watts
 BeamRadius= 12 #cm
 
@@ -160,13 +192,12 @@ samplearea = np.pi*(sampleradius**2)
 totalintensity = power/spotarea #watt/cm**2, assumes non-gaussian beam
 intensity =  totalintensity*samplearea
 
-HWHM = 1000 #wavenumbers
-CrossSection = 1e-18 #cm^2
+HWHM = 4000 #wavenumbers
+CrossSection = 1e-19 #cm^2
 absorptionPeak = 500 #nanometers
 
 h = 6.626e-34 #joule-seconds
 c = 3e8 #m/s
-e = 1.6e-19 #coulombs per electron
 
 
 #/////////////////////////////////////////////////
@@ -200,9 +231,6 @@ def plotSpectrum(*args):
         toolbar.update()
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
         
-        button = tk.Button(master=root, text="Quit", command=_quit)
-        button.pack(side=tk.BOTTOM)
-        
         canvas.mpl_connect("key_press_event", on_key_press)
     except ValueError:
         pass
@@ -235,7 +263,6 @@ def _quit():
                     # Fatal Python Error: PyEval_RestoreThread: NULL tstate
 
         
-
 ttk.Button(optionWindow, text="QUIT", command=_quit).grid(column=6, row=1, sticky='w')
 
 
